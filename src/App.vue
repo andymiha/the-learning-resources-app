d0fa805f14e127cdf7b1dfbbcefca333e0b873f9
andymiha <andreimihaescu@hotmail.com>
2024-06-17 16:07:25 -0400

    First steps

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js dodumentation.',
+          link: 'https//vue.js.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https//google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
